b1265c2f7238fd95ed89b022490a6c99e9a73f43
Javier Martín <javier@elretirao.net>
2017-09-21 20:13:06 +0200

    Remove warnings about old rspec syntax.
    While the new syntax is recommended, it sounds so strange to me that
    I'll keep using the old one as long as I don't have a better reason to
    switch to the new one.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:expect, :should]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:expect, :should]
+  end
+end
