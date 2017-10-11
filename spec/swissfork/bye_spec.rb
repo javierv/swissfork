3da3c1794f0dcfdf9cac1340d94d7e123b978281
Javier Martín <javier@elretirao.net>
2017-10-11 17:27:45 +0200

    Add more bye basic test cases.

--- spec/swissfork/bye_spec.rb
+++ spec/swissfork/bye_spec.rb
@@ -35,8 +35,8 @@ module Swissfork
         before(:each) { players[10].stub(had_bye?: true) }
 
         it "another player gets the bye" do
-          round.pair_numbers.should == [[1, 6], [2, 7], [3, 8], [4, 9], [5, 11]]
           round.bye.number.should == 10
+          round.pair_numbers.should == [[1, 6], [2, 7], [3, 8], [4, 9], [5, 11]]
         end
       end
 
@@ -46,21 +46,44 @@ module Swissfork
         end
 
         it "another player gets the bye" do
-          round.pair_numbers.should == [[1, 6], [2, 7], [3, 8], [4, 9], [5, 11]]
           round.bye.number.should == 10
+          round.pair_numbers.should == [[1, 6], [2, 7], [3, 8], [4, 9], [5, 11]]
         end
       end
 
-      context "all players in the last bracket had byes" do
+      context "penultimate bracket has an even number of players" do
         before(:each) do
           players[0..5].each { |player| player.stub(points: 1) }
           players[6..10].each { |player| player.stub(points: 0) }
-          players[6..10].each { |player| player.stub(had_bye?: true) }
         end
 
-        it "gives the bye to a player from the previous bracket" do
-          round.pair_numbers.should == [[1, 3], [2, 4], [5, 7], [8, 10], [9, 11]]
-          round.bye.number.should == 6
+        context "all players in the last bracket had byes" do
+          before(:each) do
+            players[6..10].each { |player| player.stub(had_bye?: true) }
+          end
+
+          it "gives the bye to a player from the previous bracket" do
+            round.bye.number.should == 6
+            round.pair_numbers.should == [[1, 3], [2, 4], [5, 7], [8, 10], [9, 11]]
+          end
+        end
+      end
+
+      context "penultimate bracket has an odd number of players" do
+        before(:each) do
+          players[0..6].each { |player| player.stub(points: 1) }
+          players[7..10].each { |player| player.stub(points: 0) }
+        end
+
+        context "all players in the last bracket had byes" do
+          before(:each) do
+            players[7..10].each { |player| player.stub(had_bye?: true) }
+          end
+
+          it "gives the bye to a player from the previous bracket" do
+            round.bye.number.should == 7
+            round.pair_numbers.should == [[1, 4], [2, 5], [3, 6], [8, 10], [9, 11]]
+          end
         end
       end
     end
